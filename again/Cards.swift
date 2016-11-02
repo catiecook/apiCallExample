a3e53eac4395491bd0fcffaed733902132ee3174
catiecook <catiecook888@gmail.com>
2016-11-02 11:10:52 -0600

    example of how to get API JSON data, parse it and load it so it is accessible to the whole app on the load of the first page

---
+++ again/Cards.swift
@@ -0,0 +1,24 @@
+//
+//  CardManager.swift
+//  again
+//
+//  Created by Catie Cook on 11/1/16.
+//  Copyright © 2016 ThunderWhatever. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+func loadCards(callback: @escaping (Data) -> Void){
+    
+    let searchURL = "https://jsonagainsthumanity.herokuapp.com/"
+    
+    Alamofire.request(searchURL).responseJSON(completionHandler : {
+        response in
+//        print("Got Cards")
+        callback(response.data!)
+    })
+    
+}
+
---
+++ again/LoginVC.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  again
+//
+//  Created by Ji Kim on 10/31/16.
+//  Copyright © 2016 ThunderWhatever. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class LoginVC: UIViewController {
+    
+    var blackCards = [Card]()
+    var whiteCards = [Card]()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.black
+        loadCards(callback: addToDeckCallback)
+        
+    }
+    
+    
+    func addToDeckCallback(JSONData : Data){
+        
+        let json = JSON(data: JSONData)
+        
+        for i in 0..<json["blackCards"].count {
+            
+            var card = json["blackCards"][i]
+            let text = card["text"].string
+            
+            if card["pick"] == 1 {
+                let card = Card(content: text!, type: "Black")
+                self.blackCards.append(card)
+            }
+            
+        }
+        
+        for i in 0..<json["whiteCards"].count {
+            
+            let text = json["whiteCards"][i].string
+            let card = Card(content: text!, type: "White")
+            self.whiteCards.append(card)
+            
+        }
+        
+    }
+}
+
+
+
